5b2fdf8d47fa8d50ef673cad7834658244d63331
franpatt7 <71839951+franpatt7@users.noreply.github.com>
2020-10-04 20:29:56 -0500

    Add files via upload

---
+++ Fixit7GH/Fixit7FrancesPatt.appstudio/forms/calc/calc.js
@@ -0,0 +1,12 @@
+/*
+let userNumber1 = Number(prompt("Enter a number: "))
+let userNumber2 = Number(prompt("Enter a second number:"))
+
+let summedNumber = calculate(userNumber1, userNumber2);
+
+function calculate(num1,num2){
+  return num1 + num2;
+  }
+  
+console.log(`The sum of ${userNumber1} and ${userNumber2} is ${summedNumber}`)
+*/
